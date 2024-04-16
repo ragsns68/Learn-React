afdd501d19aa61f1338525bca77699d520f441c0
ragsns68 <ragsns68@gmail.com>
2024-04-16 14:41:52 +0530

    React core code for nested elements or tags

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+/**
+ * React rendering with single children tag
+ * <div id="root">
+ * <h1 id="heading" xyz="abc">Hello World From React !</h1>
+ * </div>
+ * 
+ */
+
+
 const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World From React !")
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
