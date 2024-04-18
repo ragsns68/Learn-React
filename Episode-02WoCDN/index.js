67e36b0f4ff3974d7b6c8d32d916e6f47550c704
ragsns68 <ragsns68@gmail.com>
2024-04-18 15:51:13 +0530

    Episode-02

---
+++ Episode-02WoCDN/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const header=React.createElement("h1",{},"Hello World")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(header)
